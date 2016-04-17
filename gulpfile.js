4469138e9b95a0c9615656699198ed0c144d9a2b
Kolya Ay <kolya.ay@gmail.com>
2016-04-17 04:51:17 +0300

    Remove unused crap

--- gulpfile.js
+++ gulpfile.js
@@ -19,49 +19,6 @@ var cp = require('child_process');
 var del = require('del');
 
 var pkg = require('./package.json');
-// var options = minimist(process.argv.slice(2), {
-// 	string: ['api-path', 'api-proxy'],
-// 	default: {
-// 		'api-path': 'v1'
-// 	}
-// });
-
-var server = express();
-server.use(livereload());
-server.use(express.static('./'));
-
-var polyfills = ['array.prototype.findindex',
-                 'object.assign/dist/browser.js'];
-
-// gulp.task('script', ['patch-version'], function() {
-// 	return scriptBundle('script/index.js')
-// 		// Don't transform, see: http://git.io/vcJlV
-// 		.pipe(source('index.js')).pipe(buffer())
-// 		.pipe(plugins.sourcemaps.init({ loadMaps: true }))
-// 		.pipe(plugins.uglify())
-// 		.pipe(plugins.sourcemaps.write('./'))
-// 		.pipe(gulp.dest('dist'));
-// });
-
-// gulp.task('script-watch', ['patch-version'], function () {
-// 	return scriptBundle('script/index.js', function (bundle) {
-// 		return bundle.pipe(source('index.js'))
-// 			.pipe(gulp.dest('./dist'))
-// 			.pipe(browserSync.stream());
-// 	});
-// });
-
-// gulp.task('style', function () {
-// 	return gulp.src('style/index.less')
-// 		.pipe(plugins.sourcemaps.init())
-// 		.pipe(plugins.less({ paths: ['node_modules'] }))
-// 		// don't use less plugins due http://git.io/vqVDy bug
-// 		.pipe(plugins.autoprefixer({ browsers: ['> 0.5%'] }))
-// 		.pipe(plugins.minifyCss())
-// 		.pipe(plugins.sourcemaps.write('./'))
-// 		.pipe(gulp.dest('dist'))
-// 		.pipe(browserSync.stream({match: '**/*.css'}));
-// });
 
 gulp.task('mithril', function () {
 	var basename = 'mithril',
@@ -77,22 +34,9 @@ gulp.task('common', function () {
 		.pipe(gulp.dest('dist'));
 });
 
-// gulp.task('html', function () {
-// 	return gulp.src('index.html')
-// 		.pipe(gulp.dest('dist'))
-// 		.pipe(browserSync.stream());
-// });
-
 gulp.task('clean', function () {
 	return del(['dist/**', pkg.name + '-*.zip']);
 });
-
-// gulp.task('archive', ['assets', 'code'], function (cb) {
-// 	return gulp.src(['dist/**', '!**/*.map'])
-// 		.pipe(plugins.rename({ dirname: pkg.name }))
-// 		.pipe(plugins.zip(pkg.name + '-' + pkg.version + '.zip'))
-// 		.pipe(gulp.dest('.'));
-// });
 
 gulp.task('serve', ['assets']/*, 'style', 'script-watch']*/, function() {
 	browserSync({
@@ -100,7 +44,6 @@ gulp.task('serve', ['assets']/*, 'style', 'script-watch']*/, function() {
 		logConnections: true,
 		server: {
 			baseDir: '.'
-			//middleware: options['api-proxy'] ? proxyIfNotExist('dist', options['api-proxy']) : null
 		}
 	});
 
@@ -114,7 +57,11 @@ gulp.task('serve', ['assets']/*, 'style', 'script-watch']*/, function() {
 });
 
 gulp.task('express', ['assets']/*, 'style', 'script-watch']*/, function() {
-	server.listen(5000);
+	var server = express()
+		.use(livereload())
+		.use(express.static('./'))
+		.listen(5000);
+
 	plugins.livereload.listen();
 
 	// gulp.watch('style/**.less', ['style']);
@@ -125,83 +72,6 @@ gulp.task('express', ['assets']/*, 'style', 'script-watch']*/, function() {
 		process.exit();
 	});
 });
-
-
-gulp.task('patch-version', function (cb) {
-	if (pkg.rev)
-		return cb();
-	cp.exec('git rev-list ' + pkg.version + '..HEAD --count', function (err, stdout, _) {
-		if (err && err.code != 127) // not "command not found"
-			gutil.log('Could not fetch revision. ' +
-					  'Please git tag the package version.');
-		else if (!err && stdout > 0) {
-			pkg.rev =  stdout.toString().trim();
-			pkg.version += ('+r' + pkg.rev);
-		}
-		cb(err);
-	});
-});
-
-function scriptBundle(src, watchUpdate) {
-	var build = browserify(src, {
-		cache: {}, packageCache: {},
-		debug: true
-	});
-	build.external('codemirror')
-		.transform('exposify', { expose: {'mithril': 'm' }})
-		.transform('browserify-replace', { replace: [
-			{ from: '__VERSION__', to: pkg.version },
-			{ from: '__API_PATH__', to: options['api-path'] }
-		]});
-
-	build.on('bundle', function() {
-		var firstChunk = true;
-		var polyfillData = polyfills.reduce(function (res, module) {
-			var data = fs.readFileSync(require.resolve(module));
-			return res + data + '\n';
-		}, '');
-		var stream = through.obj(function (buf, enc, next) {
-			if (firstChunk) {
-				this.push(polyfillData);
-				firstChunk = false;
-			}
-			this.push(buf);
-			next();
-		});
-		stream.label = "prepend";
-		build.pipeline.get('wrap').push(stream);
-	});
-
-	if (!watchUpdate)
-		return build.bundle();
-
-	var rebuild = function () {
-		return watchUpdate(build.bundle().on('error', function (err) {
-			gutil.log(err.message);
-		}));
-	};
-	build.plugin(watchify);
-	build.on('log', gutil.log.bind(null, 'Script update:'));
-	build.on('update', rebuild);
-	return rebuild();
-}
-
-function proxyIfNotExist(basePath, proxyPath) {
-	var httpProxy = require('http-proxy');
-	var url = require('url');
-	var path = require('path');
-
-	var proxy = httpProxy.createProxyServer();
-	return function (req, res, next) {
-		var pn = path.join(basePath, url.parse(req.url).pathname);
-		try {
-			fs.accessSync(pn);
-			next();
-		} catch (e) {
-			proxy.web(req, res, {target: proxyPath}, function () {});
-		}
-	};
-};
 
 gulp.task('assets', ['common', 'mithril']);
 // gulp.task('code', ['style', 'script']);
